b04dc873c0de77b70ba7e693d8e495963c0bc011
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2024-03-21 17:02:05 +0530

    Added sample for loading SfListView inside the TabView
    Added sample for loading SfListView inside the TabView
    Optional extended description

---
+++ MauiListView/MauiListView/Views/BookPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MauiListView
+{
+    public partial class BookPage : ContentView
+    {
+        BookInfoRepository bookInfoRepository;
+        public BookPage()
+        {
+            InitializeComponent();
+            bookInfoRepository = new BookInfoRepository();  
+            this.BindingContext = bookInfoRepository;
+        }
+    }
+}
---
+++ MauiListView/MauiListView/Views/ContactPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MauiListView
+{
+    public partial class ContactPage : ContentView
+    {
+        public ContactPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new ContactsRepository();
+        }
+    }
+}
